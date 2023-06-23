f20958a9ba915c928d769e44af0afb0a3b2de772
Protoplosion <54971638+Protoplosion@users.noreply.github.com>
2023-06-23 19:38:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ for (var i = 0; i < numCubes; i++) {
 	var material = new THREE.MeshStandardMaterial({map: texture, emissive: 0x111111, specular: 0xffffff, roughness: 0.55});
   var cube = new THREE.Mesh(geometry, material);
   
-  var posX = window.screen.availWidth;
-  var posY = window.screen.availHeight;
+  var posX = window.screen.availWidth / 2 * 0.105;
+  var posY = window.screen.availHeight / 2 * 0.105;
   
   cube.position.x = Math.random() * (posX - -posX) + -posX;
   cube.position.y = Math.random() * (posY - -posY) + -posY;
